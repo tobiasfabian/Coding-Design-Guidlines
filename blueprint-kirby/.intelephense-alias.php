8f07a8df5f64c074a5b98436f7f01866e6a03354
Tobias Wolf <tobiasfabian@users.noreply.github.com>
2023-01-25 14:49:32 +0100

    Update .intelephense-alias.php

--- blueprint-kirby/.intelephense-alias.php
+++ blueprint-kirby/.intelephense-alias.php
@@ -1,11 +1,15 @@
 <?php
 /**
+ * Globals don’t work for me, at least I tried.
+ *
  * @global Kirby\Cms\Site $site
  * @global Kirby\Cms\Page $page
+ * @global Kirby\Cms\Block $block
  */
 
 // A little fix for intelephense
 // https://github.com/bmewburn/vscode-intelephense/issues/600
+// https://getkirby.com/docs/reference/objects/aliases
 
 // cms classes
 class Collection extends Kirby\Cms\Collection {}
@@ -69,5 +73,45 @@ class I18n       extends Kirby\Toolkit\I18n {}
 class Obj        extends Kirby\Toolkit\Obj {}
 class Str        extends Kirby\Toolkit\Str {}
 class Tpl        extends Kirby\Toolkit\Tpl {}
-class V          extends Kirby\Toolkit\V {}
 class Xml        extends Kirby\Toolkit\Xml {}
+
+
+/**
+* @method static bool accepted($value)
+* @method static bool alpha($value, bool $unicode = false)
+* @method static bool alphanum($value, bool $unicode = false)
+* @method static bool between($value, $min, $max)
+* @method static bool contains($value, $needle)
+* @method static bool date($value, string $operator = null, string $test = null)
+* @method static bool denied($value)
+* @method static bool different($value, $other, $strict = false)
+* @method static bool email($value)
+* @method static bool empty($value = null)
+* @method static bool endsWith($value, string $end)
+* @method static bool filename($value)
+* @method static bool in($value, array $in, bool $strict = false)
+* @method static bool integer($value, bool $strict = false)
+* @method static bool ip($value)
+* @method static bool json($value)
+* @method static bool less($value, float $max)
+* @method static bool match($value, string $pattern)
+* @method static bool max($value, float $max)
+* @method static bool maxLength($value, $max)
+* @method static bool maxWords($value, $max)
+* @method static bool min($value, float $min)
+* @method static bool minLength($value, $min)
+* @method static bool minWords($value, $min)
+* @method static bool more($value, float $min)
+* @method static bool notContains($value, $needle)
+* @method static bool notEmpty($value)
+* @method static bool notIn($value, $notIn)
+* @method static bool num($value)
+* @method static bool required($value, $array = null)
+* @method static bool same($value, $other, bool $strict = false)
+* @method static bool size($value, $size, $operator = '==')
+* @method static bool startsWith(string $value, string $start)
+* @method static bool time($value)
+* @method static bool url($value)
+* @method static bool uuid(string $value, string $type = null)
+*/
+class V {}
